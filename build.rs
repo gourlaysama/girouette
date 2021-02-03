810e71601ec540643341e3cdf376eb9b1bbce2c8
Antoine Gourlay <antoine@gourlay.fr>
2021-02-03 11:08:02 +0100

    feature: add autogenerated completions

---
+++ build.rs
@@ -0,0 +1,31 @@
+use std::env;
+
+use structopt::clap::Shell;
+
+include!("src/cli.rs");
+
+fn main() {
+    let outdir = match env::var_os("OUT_DIR") {
+        None => return,
+        Some(outdir) => outdir,
+    };
+    let mut app = ProgramOptions::clap();
+
+    app.gen_completions(
+        "girouette",
+        Shell::Bash,
+        &outdir,
+    );
+
+    app.gen_completions(
+        "girouette",
+        Shell::Zsh,
+        &outdir,
+    );
+
+    app.gen_completions(
+        "girouette",
+        Shell::Fish,
+        outdir,
+    );
+}
